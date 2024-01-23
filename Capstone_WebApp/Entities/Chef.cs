0dc2454c5ecb3792ab0fc4a2b64c33440830056a
Kandarp Patel <patelkandarp2002@gmail.com>
2024-01-23 18:29:21 -0500

    Project Initiation
    Adding the Project to Github. SGM.

---
+++ Capstone_WebApp/Entities/Chef.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Capstone_WebApp.Entities
+{
+	public class Chef
+	{
+		public int ChefId { get; set; }
+
+		public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+		public string? Email { get; set; }
+
+		public string? Password { get; set; }
+    }
+}
+
