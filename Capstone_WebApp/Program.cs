215acbc383a9cdaf135182445deab3dc01577144
Ayush Chakrani <ayushchakrani1234@gmail.com>
2024-01-24 18:30:14 -0500

    Added identity

--- Capstone_WebApp/Program.cs
+++ Capstone_WebApp/Program.cs
@@ -1,4 +1,12 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Capstone_WebApp.Data;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Capstone_WebAppContextConnection") ?? throw new InvalidOperationException("Connection string 'Capstone_WebAppContextConnection' not found.");
+
+builder.Services.AddDbContext<Capstone_WebAppContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<Capstone_WebAppContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
